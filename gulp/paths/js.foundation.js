83ab7ee5fc4de5f1faf33a7fe2a005b664e3eb0b
Vladimir Bun <qwelp@mail.ru>
2017-12-22 18:43:38 +0300

    template

--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = [
-  './node_modules/jquery/dist/jquery.min.js'
+  './source/js/bootstrap.min.js'
 ];
--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,40 @@
-(function() {
-  'use strict';
+$(function () {
 
-  setTimeout(function() {
-    document.querySelector('.greating_picture').classList.add('m--show');
-  }, 1000);
-})();
+	$('ul.nav li.dropdown').hover(function() {
+		$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+	}, function() {
+		$(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+	});
+
+	footer.init();
+});
+
+$(window).resize(function(){
+	footer.init();
+});
+
+var footer = (function(){
+
+	var height = function () {
+		var body = $('body'),
+			footer = body.find('.footer'),
+			footer_heigth = footer.find('.footer__layer').outerHeight(),
+			wrapper__footer = body.find('.wrapper__footer');
+
+		footer.css({
+			"height": footer_heigth + "px",
+			"margin-top": "-" + footer_heigth + "px"
+		});
+		wrapper__footer.css({
+			"height": footer_heigth + "px",
+		});
+	};
+
+	return {
+		init: function(){
+			height();
+
+
+		}
+	};
+}());
